3d0cca725a0df24461dfa2d825d8648a65f63689
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.11>
2025-03-04 05:03:57 +0700

    path

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
+use actix_web::{App, HttpServer, Result, get, web};
 
+#[get("/profile/{username}/{email}")]
+async fn index(data: web::Path<(String, String)>) -> Result<String> {
+    let (username, email) = data.into_inner();
+    Ok(format!("Username : {} \nEmail : {}", username, email))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("localhost", 4000))?
+        .run()
+        .await
+}
